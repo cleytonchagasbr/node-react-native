b8d330f5cc6f5f3c0cc7ebc73824998bda3fbab8
Cleyton Chagas <cleyton-chagas@hotmail.com>
2020-03-22 22:40:02 -0300

    add app.js and routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,17 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (request, response) => response.json({ message: 'Teste 00ok' }));
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    name: 'Novo teste',
+    email: 'cleyton.need2@gmail.com',
+    password_hash: '321654',
+  });
+
+  return response.json(user);
+});
 
 
 export default routes;
