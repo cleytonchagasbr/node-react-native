929b8408098cd6c83192fc5a5fd7c0285f92a140
Cleyton Chagas <cleyton-chagas@hotmail.com>
2020-03-23 22:35:42 -0300

    Update md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ## Projeto criado para aprimorar os estudos em Node, React e React Native
+
+### Como executar:
+- git clone
+- entre na pasta
+- yarn install
+
+- Depois entre em localhost:3333
+
+### Rotas:
+- /users
+- /sessions
+- /users
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -15,6 +15,28 @@ class UserController {
       id, name, email, provider,
     });
   }
+
+  async update(request, response) {
+
+    const { email, oldPassword } = request.body;
+
+    const user = await User.findByPk(request.userId);
+
+    if(email != user.email) {
+      const userExist = await User.findOne({ where: { email } });
+    
+      if (userExist) {
+        return response.status(400).json({ error: 'User already exists' });
+      }
+    }  
+    if(oldPassword && !(await user.checkPassword(oldPassword))) {
+      return response.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider} = await user.update(request.body);
+
+    return response.json({ id, name, email, provider });
+  }
 }
 
 export default new UserController();
