8800167288b85203afba7e1db5656114a56b2b05
Cleyton Chagas <cleyton-chagas@hotmail.com>
2020-03-25 16:34:09 -0300

    usando biblioteca Multer para upload

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,10 +1,19 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User';
 import auth from '../../config/auth';
-
+import * as Yup from 'yup';
 
 class SessionController {
     async store(request, response) {
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required(),
+            password: Yup.string().required()
+          });
+
+        if( (!await schema.isValid(request.body))) {
+            return response.status(400).json({ error: 'Validation fails'});
+        }  
+
         const { email, password } = request.body;
 
         const user = await User.findOne({ where: { email }});
