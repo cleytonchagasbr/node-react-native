dd7fa85efae0b6939db1b3c60a95ff6758d05d9e
Cleyton Chagas <cleyton-chagas@hotmail.com>
2020-03-25 18:29:28 -0300

    Relationship to Users and FIles, Add new migration.

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -24,6 +24,11 @@ class User extends Model {
     return this;
   }
 
+  static associate(models) {
+    // belongsTo = pertence a
+    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
---
+++ src/database/migrations/20200325205158-add-avatar-field-to-users.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'users',
+      'avatar_id',
+      {
+        type: Sequelize.INTEGER,
+        references: { model: 'files', key: 'id' },
+        onUpdate: 'CASCADE',
+        oneDelete: 'SET NULL',
+        allowNull: true,
+      }
+    )
+  },
+
+  down: queryInterface => {
+    return queryInterface.removeColumn('users', 'avatar_id');
+  }
+};
