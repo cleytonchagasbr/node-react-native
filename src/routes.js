768369511aaf61d9ce5606dbab0a0fa958e175c7
Cleyton Chagas <cleyton-chagas@hotmail.com>
2020-03-29 18:43:41 -0300

    ScheduleProvider

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import SheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -21,6 +22,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', SheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
