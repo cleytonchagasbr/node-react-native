c3cc9105a8993ca15987d95639f46af4bbcefcd6
Cleyton Chagas <cleyton-chagas@hotmail.com>
2020-03-29 21:33:21 -0300

    Add Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    secure: false,
+    auth: {
+      user: "14ba4305330928",
+      pass: "5462f2fd23cea4"
+    },
+    default: {
+      from: 'Cleyton Chagas <noreply@cleyton.com>',
+  },
+};
